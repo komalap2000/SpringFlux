cf29fa42a29dbe75521ff0ae776ee7b522dc868c
Kyle Rohde <krohde6@gatech.edu>
2020-05-20 11:57:48 -0400

    Hello Spring

---
+++ src/main/java/com/cisco/stealthwatch/demo/GreetingHandler.java
@@ -0,0 +1,10 @@
+package com.cisco.stealthwatch.demo;
+
+import org.springframework.stereotype.Component;
+//import org.springframework.web.reactive.function.server.ServerRequest
+
+@Component
+public class GreetingHandler {
+
+    public Mono<ServerRes>
+}
---
+++ src/test/java/com/cisco/stealthwatch/demo/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cisco.stealthwatch.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
